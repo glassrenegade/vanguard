24ed766fb993a3483684b4eaaacc8b21b6c9cdcd
Sam Gardiner <nekura.gesshoku@gmail.com>
2017-11-01 01:03:31 +0000

    got stripe and credit cards working

--- Gemfile
+++ Gemfile
@@ -57,6 +57,8 @@ gem "mini_magick"
 
 gem 'carrierwave-aws'
 
+gem 'stripe'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,25 @@
 class LessonsController < ApplicationController
-	def show
-	end
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_lesson
 
-	private
+  def show
+  end
 
-	helper_method :current_lesson
-	def current_lesson
-		@current_lesson ||= Lesson.find(params[:id])
-	end
+  private
+
+  def require_enrollment_for_lesson
+    if current_user.enrolled_in?(current_course) == false
+      redirect_to course_path(current_course), alert: 'You must be enrolled'      
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 end
